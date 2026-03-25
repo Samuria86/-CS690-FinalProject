dcaa2f68657da0f311f0feedf106480c44d8daf9
Evander Chow <60991886+Samuria86@users.noreply.github.com>
2026-03-25 01:57:07 +0000

    Google Books API implementation

---
+++ Virtual_Bookshelf/Library.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Books.Services;
+using System.Diagnostics;
+
+namespace Library.UnitTests.Services
+{
+
+    public class BookSearchTest
+    {
+        [Fact]  
+        public async Task TestISBNSearch()
+        {
+            string isbn = "0071807993";
+            string parameter = "isbn";
+            var result = await BookService.SearchBook(parameter, isbn);
+            Assert.NotNull(result);
+            Console.WriteLine("\nBook Name: " + result.VolumeInfo.Title);
+            Console.WriteLine("Authors: " + string.Join(", ", result.VolumeInfo.Authors));
+            Console.WriteLine("Publisher: " + result.VolumeInfo.Publisher);
+        }
+        [Fact]
+        public async Task TestTitleSearch()
+        {
+            string title = "The Great Gatsby";
+            string parameter = "intitle";
+            var result = await BookService.SearchBook(parameter, title);
+            Assert.NotNull(result);
+            Console.WriteLine("\nBook Name: " + result.VolumeInfo.Title);
+            Console.WriteLine("Authors: " + string.Join(", ", result.VolumeInfo.Authors));
+            Console.WriteLine("Publisher: " + result.VolumeInfo.Publisher);
+        }
+    }
+    
+}
---
+++ Virtual_Bookshelf/Library/Program.cs
@@ -0,0 +1,37 @@
+﻿using DotNetEnv;
+using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+using Google.Apis.Services;
+using System.Linq;
+
+Env.Load(); // Loads the .env file from the current directory
+
+namespace Books.Services
+{
+    public static class BookService
+    {
+        public string apiKey = Environment.GetEnvironmentVariable("GOOGLE_BOOKS_API_KEY");
+       
+        public static BooksService service = new BooksService(
+            new BaseClientService.Initializer
+        {
+                ApplicationName = "ISBNBookSearch",
+                ApiKey = apiKey,
+        });
+
+        public static async Task<Volume> SearchBook(string parameter, string input)
+        {
+            Console.WriteLine("Executing a book search request...");
+            var result = await service.Volumes.List(parameter + ":" + input).ExecuteAsync();
+            if (result != null && result.Items != null)
+            {
+                var item = result.Items.FirstOrDefault();
+                return item;
+            }
+            return null;
+        }
+    }
+}
+
+
+
